8c10c55a3ac912625f3af99dd077046944da5f55
Karnav <64678161+kkkarnav@users.noreply.github.com>
2025-06-29 19:25:26 +0530

    updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # indian_disapora
 
+ - Download the CampaignFinance files from OpenSecrets
+ - First, run data_agg.py to aggregate the data from transaction-level to donor-cycle level
+ - Then, run class_lastname.ipynb and/or classif_model.ipynb to produce the name classifications
--- data_agg.py
+++ data_agg.py
@@ -17,10 +17,21 @@ warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", help="Path to the indivs csv", type=str, default="./data/CampaignFin20/indivs20.txt")
+parser.add_argument("-t", "--task", help="Task to run/generate", type=str, default="donors")
 args = parser.parse_args()
 
 os_csv = args.path
 year = os_csv.split(".")[-2][-2:]
+
+
+# for 2016
+'''with open(os_csv, 'r', encoding='utf-8', errors='replace') as f:
+    content = f.read()
+    content = content.replace('|15| ', '|15 |')  # or whatever fix you're trying
+    
+temp_file = "temp_fixed.csv"
+with open(temp_file, 'w', encoding='utf-8') as f:
+    f.write(content)'''
 
 lf = (
         pl.scan_csv(
@@ -54,29 +65,88 @@ lf = (
         ])
     )
 
-df = lf.collect(streaming=True)
-print(df.head(10))
+# df = lf.collect(streaming=True)
+# print(df.head(10))
 
-donors = (
-    df.group_by("contrib_id")
-    .agg(
-        pl.col("name").first().alias("name"),
-        pl.col("name_new").first().alias("name_new"),
-        pl.col("orgname").first().alias("orgname"),
-        pl.col("ultorg").first().alias("ultorg"),
-        pl.col("gender").first().alias("gender"),
-        pl.col("occupation").first().alias("occupation"),
-        pl.col("employer").first().alias("employer"),
-        pl.col("amount").sum().alias("total_donated"),
-        pl.col("amount").count().alias("donation_count"),
-        pl.col("amount").mean().alias("avg_donation"),
-        pl.col("amount").median().alias("med_donation"),
+
+if args.task == "donors":
+    
+    print("Aggregating donors...")
+    
+    donors = (
+        lf.group_by("contrib_id")
+        .agg(
+            pl.col("name").mode().first().alias("name"),
+            pl.col("name_new").mode().first().alias("name_new"),
+            pl.col("orgname").mode().first().alias("orgname"),
+            pl.col("ultorg").mode().first().alias("ultorg"),
+            pl.col("realcode").mode().first().alias("realcode"),
+            pl.col("gender").mode().first().alias("gender"),
+            pl.col("occupation").mode().first().alias("occupation"),
+            pl.col("employer").mode().first().alias("employer"),
+            pl.col("amount").sum().alias("total_donated"),
+            pl.col("amount").count().alias("donation_count"),
+            pl.col("amount").mean().alias("avg_donation"),
+            pl.col("amount").median().alias("med_donation"),
+        )
+        .sort("total_donated", descending=True)
+        .collect(streaming=True)
     )
-    .sort("total_donated", descending=True)
+
+    donors.write_csv(f"./data/CampaignFin{year}/donors{year}.csv")
+    print(donors.tail(10))
+
+    donors[:10000].write_csv(f"./data/CampaignFin{year}/top_donors{year}.csv")
+    print(donors.head(10))
+
+
+if args.task == "donors_state":
+    
+    print("Aggregating donors with state info...")
+    
+    donors_state = (
+        lf.group_by("contrib_id")
+        .agg(
+            pl.col("name").mode().first().alias("name"),
+            pl.col("name_new").mode().first().alias("name_new"),
+            pl.col("orgname").mode().first().alias("orgname"),
+            pl.col("ultorg").mode().first().alias("ultorg"),
+            pl.col("realcode").mode().first().alias("realcode"),
+            pl.col("gender").mode().first().alias("gender"),
+            pl.col("occupation").mode().first().alias("occupation"),
+            pl.col("employer").mode().first().alias("employer"),
+            pl.col("city").mode().first().alias("city"),
+            pl.col("state").mode().first().alias("state"),
+            pl.col("amount").sum().alias("total_donated"),
+            pl.col("amount").count().alias("donation_count"),
+            pl.col("amount").mean().alias("avg_donation"),
+            pl.col("amount").median().alias("med_donation"),
+        )
+        .sort("total_donated", descending=True)
+        .collect(streaming=True)
+    )
+
+    donors_state.write_csv(f"./data/CampaignFin{year}/donors_state{year}.csv")
+    print(donors_state.tail(10))
+
+    donors_state[:10000].write_csv(f"./data/CampaignFin{year}/top_donors_state{year}.csv")
+    print(donors_state.head(10))
+
+
+if args.task == "state":
+    
+    print("Aggregating donors...")
+    
+    state_stats = (
+    lf.group_by("state")
+    .agg(
+        pl.col("amount").mean().alias("avg_amount"),
+        pl.col("amount").median().alias("med_amount"),
+        pl.col("amount").count().alias("total_donations"),
+        pl.col("amount").filter(pl.col("ethnic") == "ind").mean().alias("avg_amount_indian"),
+        pl.col("amount").filter(pl.col("ethnic") == "ind").median().alias("med_amount_indian"),
+        pl.col("amount").filter(pl.col("ethnic") == "ind").count().alias("indian_donations")
+    )
+    .sort("avg_amount", descending=True)
+    .collect(streaming=True)
 )
-
-donors.write_csv(f"./data/CampaignFin{year}/donors{year}.csv")
-print(donors.tail(10))
-
-donors[:10000].write_csv(f"./data/CampaignFin{year}/top_donors{year}.csv")
-print(donors.head(10))
